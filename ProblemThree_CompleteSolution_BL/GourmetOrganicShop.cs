76ada85417f98ab615681b40bf8faf634d45dd26
chandana <chandanasraj077@gmail.com>
2021-09-02 17:00:13 +0530

    commit

--- ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetOrganicShop.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace ProblemThree_CompleteSolution_BL
 {
-    public class GourmetOrganicShop : GourmetShop,ITaxCompute
+    public class GourmetOrganicShop : GourmetShop, ITaxCompute
     {
-        
+       public  Dictionary<int, double> d = new Dictionary<int, double>();
+       public int itemno;
+       public string ItemName;
+        public double pp;
+        public double pw;
+
+        public GourmetOrganicShop(int a, string b, Dictionary<int,double> c)
+        {
+            itemno = a;
+            ItemName = b;
+            foreach(KeyValuePair<int,double> items in c)
+            {
+                d.Add(items.Key,items.Value);
+            }
+        }
+        public override double PayPerPiece(int quantity)
+        {
+           
+            pp= quantity * d.ElementAt(itemno).Value;
+            return pp;
+                
+
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            return weight * d.ElementAt(itemno).Value;
+        }
+        public double TaxOnTotalBill()
+        {
+
+            return 0.2 * pp;
+        }
     }
 }
